d8fc816bc1e4d1e8a064e8b2a6fefe83d26ead39
Hamza Ibrahim <114664971+Hamzawypash@users.noreply.github.com>
2025-01-27 00:22:52 +0000

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+const togglebutton = document.getElementsByClassName('toggle-button')[0]
+const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+ 
+togglebutton.addEventListener('click',() => {
+   navbarlinks.classList.toggle('active')
+})
+const text = document.querySelector('.animated-text');
+ 
+setInterval(() => {
+  const colors = ['#FFD301'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  text.style.color = randomColor;
+}, 500);
+ 
